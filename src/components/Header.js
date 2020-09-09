479a08468e6052276992ecf3710e18cd8ed32c9b
fluturecode <fluturecode@gmail.com>
2020-09-09 11:46:21 -0400

    added products page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,47 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+	const [{ basket }, dispatch] = useStateValue();
+
 	return (
 		<div className="header">
-			<img src="http://pngimg.com/uplads/amazon/amazon_PNG11.png" />
+			<Link to="/">
+				<img
+					src="https://www.nicepng.com/png/full/228-2281836_vault-logo-available-amazon-app-store.png"
+					alt="Amazon Logo White"
+					className="header__logo"
+				/>
+			</Link>
 			<div className="header__search">
-				<input className="header__searchInput" type="text" />
+				<input type="search" className="header__searchInput" />
+				<SearchIcon className="header__searchIcon" />
 			</div>
 			<div className="header__nav">
-				<div className="header__option"></div>
-				<div className="header__option"></div>
-				<div className="header__option"></div>
-				<div className="header__option"></div>
+				<div className="header__option">
+					<span className="header__optionLineOne">Hello Guest</span>
+					<span className="header__optionLineTwo">Sign In</span>
+				</div>
+				<div className="header__option">
+					<span className="header__optionLineOne">Returns</span>
+					<span className="header__optionLineTwo">&amp; Orders</span>
+				</div>
+				<div className="header__option">
+					<span className="header__optionLineOne">Your</span>
+					<span className="header__optionLineTwo">prime</span>
+				</div>
+				<Link to="/checkout">
+					<div className="header__optionBasket">
+						<ShoppingBasketIcon />
+						<span className="header__optionLineTwo header__basketCount">
+							{basket?.length}
+						</span>
+					</div>
+				</Link>
 			</div>
 		</div>
 	);
