f17dbf4f2f7a4153eeacacc87b32dd4af1c558e1
fluturecode <fluturecode@gmail.com>
2020-09-10 20:57:23 -0400

    added backend & user auth

---
+++ build/precache-manifest.a97fbbc39d586a3d521586941d243e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d8d598ec25c8ce0a4acce607718a82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37e0e7c606d10aeec044",
+    "url": "/static/css/main.ccea6e14.chunk.css"
+  },
+  {
+    "revision": "27c9cc02286d59362049",
+    "url": "/static/js/2.99ff1bc3.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.99ff1bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e0e7c606d10aeec044",
+    "url": "/static/js/main.103cfd2e.chunk.js"
+  },
+  {
+    "revision": "7646288671f8458d76de",
+    "url": "/static/js/runtime-main.49627031.js"
+  }
+]);
