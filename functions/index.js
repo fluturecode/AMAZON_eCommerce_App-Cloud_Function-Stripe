110c76a0db3c7ec93c6eff0e297930469b0bebae
fluturecode <fluturecode@gmail.com>
2020-09-11 22:36:57 -0400

    added backend server

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,34 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+// Add stripe secret key from .env
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// App config
+const app = express();
+
+// Middleware
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+app.get("/", (req, res) => res.status(200).send("Helloooo"));
+
+app.post("/payments/create", async (req, res) => {
+	const total = req.query.total;
+
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: total, //subunits of currency
+		currency: "usd",
+	});
+	res.status(201).send({
+		clientSecret: paymentIntent.client_secret,
+	});
+});
+
+// Listen command
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint:
+// http://localhost:5001/fir-c12e4/us-central1/api
