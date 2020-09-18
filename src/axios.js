888dcedac828aea56ceb0b8fbf7ec182c277d5f9
Elizabeth Eidelson <fluturecode@gmail.com>
2020-09-18 14:29:58 -0400

    fixed all bugs

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
 	// THE API (cloud function) URL
-	baseURL: "https://localhost:5001/fir-c12e4/us-central1/api",
+	baseURL: "https://fir-c12e4.firebaseio.com",
 });
 
 export default instance;
